e12e54b1abd275619975a599eb3be185434156b0
Ramji Gautam <ramji.gautam24@gmail.com>
2019-01-09 20:44:21 +0545

    Chrome passview first commit

---
+++ PassView/Chrome.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+using System.IO;
+
+namespace PassView
+{
+    public class Chrome
+    {
+        private static string CopySourceFile() {
+            string LoginDataSource = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)
+                + "/Google/Chrome/User Data/Default";///Login Data"; // a path to the file
+            string LoginDataDestination = "";
+            if (Directory.Exists(LoginDataSource))
+            {
+                string[] files = Directory.GetFiles(LoginDataSource);
+
+                // Copy the files and overwrite destination files if they already exist.
+                foreach (string s in files)
+                {
+                    // Use static Path methods to extract only the file name from the path.
+                    string fileName = Path.GetFileName(s);
+                    if(fileName == "Login Data")
+                    {
+                        LoginDataDestination = fileName;
+                        File.Copy(s, LoginDataDestination, true);
+                    }
+                    
+                }
+                return LoginDataDestination;
+            }
+            else
+            {
+                Console.WriteLine("Source path does not exist!");
+            }
+            return null;
+        }
+
+        public static DataTable GetPassData(bool keepEncpFile = false) {
+            string LoginData = CopySourceFile();
+            string db_field = "logins";   // DB table field name
+
+            string ConnectionString = "data source=" + LoginData + ";New=True;UseUTF16Encoding=True";
+            DataTable DB = new DataTable();
+            string sql = string.Format("SELECT * FROM {0} {1} {2}", db_field, "", "");
+            using (SQLiteConnection connect = new SQLiteConnection(ConnectionString))
+            {
+                SQLiteCommand command = new SQLiteCommand(sql, connect);
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter(command);
+                adapter.Fill(DB);
+            }
+            if (!keepEncpFile)
+                File.Delete(LoginData);
+            return DB;
+        }
+    }
+}
---
+++ PassView/PassView.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+using System.IO;
+using System.Text;
+
+namespace PassView
+{
+    class PassView
+    {
+        static void Main(string[] args)
+        {
+            byte[] entropy = null; // DPAPI class does not use entropy but requires this parameter
+            string description;    // I could not understand the purpose of a this mandatory parameter
+                                   // Output always is Null
+            string filename = "my_chrome_passwords.html";
+            StreamWriter Writer = new StreamWriter(filename, false, Encoding.UTF8);
+
+            DataTable DB = Chrome.GetPassData(true);
+
+            int rows = DB.Rows.Count;
+            Writer.Write("<table><thead><tr><th>S.No</th><th>Site URL</th><th>User</th><th>Password</th></tr></thead><tbody>");
+            for (int i = 0; i < rows; i++)
+            {
+                byte[] byteArray = (byte[])DB.Rows[i][5];
+                byte[] decrypted = EncoderDecoder.Decrypt(byteArray, entropy, out description);
+                string password = new UTF8Encoding(true).GetString(decrypted);
+
+                Writer.Write($"<tr><td>{i + 1}</td><td>{DB.Rows[i][1]}</td><td>{DB.Rows[i][3]}</td><td>{password}</td></tr>");
+            }
+            Writer.Write("</tbody></table>");
+            Writer.Close();
+        }
+    }
+}
